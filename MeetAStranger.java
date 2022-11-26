2fb0ee4b92d200fabfc3636616ef1fb10f9acd5a
Maria Dymsha <103815224+dymshamaria@users.noreply.github.com>
2022-11-26 16:57:12 +0300

    Task

--- MeetAStranger.java
+++ MeetAStranger.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 
 public class MeetAStranger {
     public static void main(String[] args) {
-        //Write a program, which read a String from System.in and print "Hello, <input string>"
+        
+        //Write a program, which read a String from System.in and print "Hello, <input string>"     
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
         System.out.println("Hello, " + input);
